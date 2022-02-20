0b3f37986e1c1e94a1b5da56833b340536db7596
lzg119 <linzhiguan119@126.com>
2022-02-20 19:39:11 +0800

    [特性新增] 初始化版本，有人物移动功能

---
+++ BaseHuman.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHuman : MonoBehaviour{
+    protected bool ismoving = false;
+    private Vector3 targetPosition;
+    public float speed = 1.2f;
+    private Animator animator;
+    // Start is called before the first frame update
+    protected void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected void Update(){
+        MoveUpdate();
+    }
+    public void MoveTo(Vector3 pos){
+        targetPosition = pos;
+        ismoving = true;
+        animator.SetBool("isMoving", true);
+    }
+    public void MoveUpdate(){
+        if(ismoving == false)
+            return;
+
+        Vector3 pos = transform.position;
+        transform.position = Vector3.MoveTowards(pos, targetPosition, speed * Time.deltaTime);
+        transform.LookAt(targetPosition);
+        if(Vector3.Distance(pos, targetPosition) < 0.05f){
+            ismoving = false;
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
---
+++ CtrlHuman.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlHuman : BaseHuman {
+    // Start is called before the first frame update
+	new void Start () {
+		base.Start();
+	}
+
+    // Update is called once per frame
+    new void Update(){
+        base.Update();
+        if(Input.GetMouseButtonDown(0)){
+            Debug.Log("mouse gogo");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            Debug.Log(hit.collider.tag);
+            if(hit.collider.tag == "Terrain"){
+                MoveTo(hit.point);
+            }
+        }
+    }
+
+}
